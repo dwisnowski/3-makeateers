5d05709c8c0953782f478f58f385b3735979773e
David Wisnowski <dwisnows@gmail.com>
2025-02-09 11:44:57 -0600

    feat: add trivia questions for the Tron ride at Magic Kingdom

---
+++ app/shared/parks/magic_kingdom/rides/tron.py
@@ -0,0 +1,102 @@
+questions = [
+    {
+        "question": "What is the full name of the Tron ride at Disney World?",
+        "answers": ["TRON Lightcycle / Run", "TRON Lightcycle Power Run", "TRON Lightcycle Adventure", "TRON Lightcycle Coaster"],
+        "correct_answer": "TRON Lightcycle / Run"
+    },
+    {
+        "question": "Which Disney World park is home to the Tron ride?",
+        "answers": ["Magic Kingdom", "Epcot", "Hollywood Studios", "Animal Kingdom"],
+        "correct_answer": "Magic Kingdom"
+    },
+    {
+        "question": "In which area of Magic Kingdom is the Tron ride located?",
+        "answers": ["Tomorrowland", "Fantasyland", "Adventureland", "Frontierland"],
+        "correct_answer": "Tomorrowland"
+    },
+    {
+        "question": "What is the theme of the Tron ride?",
+        "answers": ["Futuristic digital world", "Underwater adventure", "Jungle exploration", "Haunted house"],
+        "correct_answer": "Futuristic digital world"
+    },
+    {
+        "question": "What type of ride is the Tron ride?",
+        "answers": ["Motorbike roller coaster", "Dark ride", "Water ride", "Simulated ride"],
+        "correct_answer": "Motorbike roller coaster"
+    },
+    {
+        "question": "What is the height requirement to ride Tron?",
+        "answers": ["48 inches", "44 inches", "52 inches", "40 inches"],
+        "correct_answer": "48 inches"
+    },
+    {
+        "question": "What is the maximum speed of the Tron ride?",
+        "answers": ["59 mph", "50 mph", "45 mph", "40 mph"],
+        "correct_answer": "59 mph"
+    },
+    {
+        "question": "What is the duration of the Tron ride?",
+        "answers": ["2 minutes", "3 minutes", "4 minutes", "5 minutes"],
+        "correct_answer": "2 minutes"
+    },
+    {
+        "question": "What is the name of the Tron ride in Shanghai Disneyland?",
+        "answers": ["TRON Lightcycle Power Run", "TRON Lightcycle / Run", "TRON Lightcycle Adventure", "TRON Lightcycle Coaster"],
+        "correct_answer": "TRON Lightcycle Power Run"
+    },
+    {
+        "question": "What is the seating arrangement on the Tron ride?",
+        "answers": ["Motorbike-style", "Side by side", "Single file", "Back to back"],
+        "correct_answer": "Motorbike-style"
+    },
+    {
+        "question": "What is the name of the digital world featured in the Tron ride?",
+        "answers": ["The Grid", "The Matrix", "The Digital Realm", "The Cyberverse"],
+        "correct_answer": "The Grid"
+    },
+    {
+        "question": "Who is the main character you follow in the Tron ride?",
+        "answers": ["Sam Flynn", "Kevin Flynn", "Quorra", "Tron"],
+        "correct_answer": "Sam Flynn"
+    },
+    {
+        "question": "What is the name of the lightcycles used in the Tron ride?",
+        "answers": ["Lightcycles", "Speeders", "Hoverbikes", "Cybercycles"],
+        "correct_answer": "Lightcycles"
+    },
+    {
+        "question": "What is the name of the antagonist in the Tron ride?",
+        "answers": ["CLU", "Rinzler", "Sark", "Dillinger"],
+        "correct_answer": "CLU"
+    },
+    {
+        "question": "What is the name of the company that created the digital world in Tron?",
+        "answers": ["ENCOM", "Flynn's Arcade", "ISO", "TRON Corp"],
+        "correct_answer": "ENCOM"
+    },
+    {
+        "question": "What is the name of the original Tron movie's director?",
+        "answers": ["Steven Lisberger", "James Cameron", "George Lucas", "Ridley Scott"],
+        "correct_answer": "Steven Lisberger"
+    },
+    {
+        "question": "What year was the original Tron movie released?",
+        "answers": ["1982", "1985", "1990", "1995"],
+        "correct_answer": "1982"
+    },
+    {
+        "question": "What is the name of the sequel to the original Tron movie?",
+        "answers": ["Tron: Legacy", "Tron: Evolution", "Tron: Reboot", "Tron: Resurrection"],
+        "correct_answer": "Tron: Legacy"
+    },
+    {
+        "question": "What year was Tron: Legacy released?",
+        "answers": ["2010", "2008", "2012", "2015"],
+        "correct_answer": "2010"
+    },
+    {
+        "question": "What is the name of the actor who plays Kevin Flynn in both Tron movies?",
+        "answers": ["Jeff Bridges", "Bruce Boxleitner", "Garrett Hedlund", "Michael Sheen"],
+        "correct_answer": "Jeff Bridges"
+    }
+]
