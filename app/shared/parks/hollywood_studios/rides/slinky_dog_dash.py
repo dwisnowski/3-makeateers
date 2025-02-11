cc0a5e1e7b30ca8ee37416a6b25ddfe83253cef0
David Wisnowski <dwisnows@gmail.com>
2025-02-10 20:02:06 -0600

    slinky

---
+++ app/shared/parks/hollywood_studios/rides/slinky_dog_dash.py
@@ -0,0 +1,104 @@
+ride_name = "Slinky Dog Dash"
+
+questions = [
+    {
+        "question": "What year did Slinky Dog Dash open at Disney's Hollywood Studios?",
+        "answers": ["2018", "2017", "2019", "2020"],
+        "correct_answer": "2018"
+    },
+    {
+        "question": "Which Disney World park is home to Slinky Dog Dash?",
+        "answers": ["Hollywood Studios", "Magic Kingdom", "Epcot", "Animal Kingdom"],
+        "correct_answer": "Hollywood Studios"
+    },
+    {
+        "question": "What is the theme of Slinky Dog Dash?",
+        "answers": ["Toy Story", "Star Wars", "Cars", "Finding Nemo"],
+        "correct_answer": "Toy Story"
+    },
+    {
+        "question": "Who designed Slinky Dog Dash?",
+        "answers": ["Walt Disney Imagineering", "Tony Baxter", "John Hench", "Bob Gurr"],
+        "correct_answer": "Walt Disney Imagineering"
+    },
+    {
+        "question": "What is the height requirement to ride Slinky Dog Dash?",
+        "answers": ["38 inches", "40 inches", "44 inches", "48 inches"],
+        "correct_answer": "38 inches"
+    },
+    {
+        "question": "What is the maximum speed of Slinky Dog Dash?",
+        "answers": ["40 mph", "35 mph", "45 mph", "50 mph"],
+        "correct_answer": "40 mph"
+    },
+    {
+        "question": "What is the duration of the Slinky Dog Dash ride?",
+        "answers": ["2 minutes", "1 minute 30 seconds", "2 minutes 30 seconds", "3 minutes"],
+        "correct_answer": "2 minutes"
+    },
+    {
+        "question": "What type of ride is Slinky Dog Dash?",
+        "answers": ["Family roller coaster", "Dark ride", "Simulated ride", "Boat ride"],
+        "correct_answer": "Family roller coaster"
+    },
+    {
+        "question": "What is the name of the character that the ride vehicle is themed after in Slinky Dog Dash?",
+        "answers": ["Slinky Dog", "Buzz Lightyear", "Woody", "Jessie"],
+        "correct_answer": "Slinky Dog"
+    },
+    {
+        "question": "What is the name of the land where Slinky Dog Dash is located?",
+        "answers": ["Toy Story Land", "Star Wars: Galaxy's Edge", "Cars Land", "Pixar Pier"],
+        "correct_answer": "Toy Story Land"
+    },
+    {
+        "question": "What is the name of the toy that Slinky Dog Dash is based on?",
+        "answers": ["Slinky", "Buzz Lightyear", "Woody", "Jessie"],
+        "correct_answer": "Slinky"
+    },
+    {
+        "question": "What is the name of the song played during the ride in Slinky Dog Dash?",
+        "answers": ["You've Got a Friend in Me", "We Belong Together", "Strange Things", "I Will Go Sailing No More"],
+        "correct_answer": "You've Got a Friend in Me"
+    },
+    {
+        "question": "What is the name of the character who gives the safety instructions in Slinky Dog Dash?",
+        "answers": ["Woody", "Buzz Lightyear", "Jessie", "Rex"],
+        "correct_answer": "Woody"
+    },
+    {
+        "question": "What is the name of the character who appears at the end of the ride in Slinky Dog Dash?",
+        "answers": ["Wheezy", "Buzz Lightyear", "Woody", "Jessie"],
+        "correct_answer": "Wheezy"
+    },
+    {
+        "question": "What is the name of the toy company that makes Slinky Dog?",
+        "answers": ["James Industries", "Mattel", "Hasbro", "Fisher-Price"],
+        "correct_answer": "James Industries"
+    },
+    {
+        "question": "What is the name of the character who is a cowboy in Toy Story?",
+        "answers": ["Woody", "Buzz Lightyear", "Jessie", "Bullseye"],
+        "correct_answer": "Woody"
+    },
+    {
+        "question": "What is the name of the character who is a space ranger in Toy Story?",
+        "answers": ["Buzz Lightyear", "Woody", "Jessie", "Rex"],
+        "correct_answer": "Buzz Lightyear"
+    },
+    {
+        "question": "What is the name of the character who is a cowgirl in Toy Story?",
+        "answers": ["Jessie", "Woody", "Buzz Lightyear", "Bullseye"],
+        "correct_answer": "Jessie"
+    },
+    {
+        "question": "What is the name of the character who is a dinosaur in Toy Story?",
+        "answers": ["Rex", "Woody", "Buzz Lightyear", "Jessie"],
+        "correct_answer": "Rex"
+    },
+    {
+        "question": "What is the name of the character who is a piggy bank in Toy Story?",
+        "answers": ["Hamm", "Woody", "Buzz Lightyear", "Jessie"],
+        "correct_answer": "Hamm"
+    }
+]
