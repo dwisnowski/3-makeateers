c9c613535e98a42829f2f3b039013edf61eadb7a
David Wisnowski <dwisnows@gmail.com>
2025-02-10 20:07:21 -0600

    more rides

---
+++ app/shared/parks/epcot/rides/guardians_of_the_galaxy_cosmic_rewind.py
@@ -0,0 +1,104 @@
+ride_name = "Guardians of the Galaxy: Cosmic Rewind"
+
+questions = [
+    {
+        "question": "What year did Guardians of the Galaxy: Cosmic Rewind open at EPCOT?",
+        "answers": ["2022", "2021", "2020", "2019"],
+        "correct_answer": "2022"
+    },
+    {
+        "question": "Which Disney World park is home to Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["EPCOT", "Magic Kingdom", "Hollywood Studios", "Animal Kingdom"],
+        "correct_answer": "EPCOT"
+    },
+    {
+        "question": "What is the theme of Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Guardians of the Galaxy", "Star Wars", "Avengers", "Spider-Man"],
+        "correct_answer": "Guardians of the Galaxy"
+    },
+    {
+        "question": "Who designed Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Walt Disney Imagineering", "Tony Baxter", "John Hench", "Bob Gurr"],
+        "correct_answer": "Walt Disney Imagineering"
+    },
+    {
+        "question": "What is the height requirement to ride Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["42 inches", "40 inches", "44 inches", "48 inches"],
+        "correct_answer": "42 inches"
+    },
+    {
+        "question": "What is the maximum speed of Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["60 mph", "55 mph", "50 mph", "45 mph"],
+        "correct_answer": "60 mph"
+    },
+    {
+        "question": "What is the duration of the Guardians of the Galaxy: Cosmic Rewind ride?",
+        "answers": ["3 minutes", "4 minutes", "5 minutes", "6 minutes"],
+        "correct_answer": "4 minutes"
+    },
+    {
+        "question": "What type of ride is Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Indoor roller coaster", "Dark ride", "Simulated ride", "Boat ride"],
+        "correct_answer": "Indoor roller coaster"
+    },
+    {
+        "question": "What is the name of the ship featured in Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Milano", "Benatar", "Star Blaster", "Dark Aster"],
+        "correct_answer": "Milano"
+    },
+    {
+        "question": "Who is the main antagonist in Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Eson the Celestial", "Thanos", "Ronan the Accuser", "Ego the Living Planet"],
+        "correct_answer": "Eson the Celestial"
+    },
+    {
+        "question": "What is the name of the planet where Guardians of the Galaxy: Cosmic Rewind is set?",
+        "answers": ["Xandar", "Earth", "Knowhere", "Sakaar"],
+        "correct_answer": "Xandar"
+    },
+    {
+        "question": "What is the name of the team that the Guardians of the Galaxy belong to?",
+        "answers": ["Guardians of the Galaxy", "Avengers", "X-Men", "Fantastic Four"],
+        "correct_answer": "Guardians of the Galaxy"
+    },
+    {
+        "question": "What is the name of the character who is a raccoon in Guardians of the Galaxy?",
+        "answers": ["Rocket", "Groot", "Star-Lord", "Drax"],
+        "correct_answer": "Rocket"
+    },
+    {
+        "question": "What is the name of the character who is a tree in Guardians of the Galaxy?",
+        "answers": ["Groot", "Rocket", "Star-Lord", "Drax"],
+        "correct_answer": "Groot"
+    },
+    {
+        "question": "What is the name of the character who is the leader of the Guardians of the Galaxy?",
+        "answers": ["Star-Lord", "Rocket", "Groot", "Drax"],
+        "correct_answer": "Star-Lord"
+    },
+    {
+        "question": "What is the name of the character who is a green-skinned assassin in Guardians of the Galaxy?",
+        "answers": ["Gamora", "Nebula", "Mantis", "Yondu"],
+        "correct_answer": "Gamora"
+    },
+    {
+        "question": "What is the name of the character who is a blue-skinned warrior in Guardians of the Galaxy?",
+        "answers": ["Nebula", "Gamora", "Mantis", "Yondu"],
+        "correct_answer": "Nebula"
+    },
+    {
+        "question": "What is the name of the character who is a telepathic empath in Guardians of the Galaxy?",
+        "answers": ["Mantis", "Gamora", "Nebula", "Yondu"],
+        "correct_answer": "Mantis"
+    },
+    {
+        "question": "What is the name of the character who is a blue-skinned Ravager in Guardians of the Galaxy?",
+        "answers": ["Yondu", "Nebula", "Gamora", "Mantis"],
+        "correct_answer": "Yondu"
+    },
+    {
+        "question": "What is the name of the song played during the ride in Guardians of the Galaxy: Cosmic Rewind?",
+        "answers": ["Hooked on a Feeling", "Come and Get Your Love", "Mr. Blue Sky", "I Want You Back"],
+        "correct_answer": "Hooked on a Feeling"
+    }
+]
---
+++ app/shared/parks/epcot/rides/test_track.py
@@ -0,0 +1,104 @@
+ride_name = "Test Track"
+
+questions = [
+    {
+        "question": "What year did Test Track open at EPCOT?",
+        "answers": ["1999", "1998", "2000", "2001"],
+        "correct_answer": "1999"
+    },
+    {
+        "question": "Which Disney World park is home to Test Track?",
+        "answers": ["EPCOT", "Magic Kingdom", "Hollywood Studios", "Animal Kingdom"],
+        "correct_answer": "EPCOT"
+    },
+    {
+        "question": "What is the theme of Test Track?",
+        "answers": ["Automobile testing", "Space travel", "Underwater exploration", "Aviation"],
+        "correct_answer": "Automobile testing"
+    },
+    {
+        "question": "Who designed Test Track?",
+        "answers": ["Walt Disney Imagineering", "Tony Baxter", "John Hench", "Bob Gurr"],
+        "correct_answer": "Walt Disney Imagineering"
+    },
+    {
+        "question": "What is the height requirement to ride Test Track?",
+        "answers": ["40 inches", "38 inches", "44 inches", "48 inches"],
+        "correct_answer": "40 inches"
+    },
+    {
+        "question": "What is the maximum speed of Test Track?",
+        "answers": ["65 mph", "60 mph", "55 mph", "50 mph"],
+        "correct_answer": "65 mph"
+    },
+    {
+        "question": "What is the duration of the Test Track ride?",
+        "answers": ["5 minutes", "4 minutes", "6 minutes", "7 minutes"],
+        "correct_answer": "5 minutes"
+    },
+    {
+        "question": "What type of ride is Test Track?",
+        "answers": ["High-speed slot car ride", "Roller coaster", "Simulated ride", "Boat ride"],
+        "correct_answer": "High-speed slot car ride"
+    },
+    {
+        "question": "What is the name of the company that sponsors Test Track?",
+        "answers": ["Chevrolet", "Ford", "Toyota", "Honda"],
+        "correct_answer": "Chevrolet"
+    },
+    {
+        "question": "What is the name of the area where you design your own car in Test Track?",
+        "answers": ["Design Studio", "Car Creation Center", "Vehicle Design Lab", "Automobile Workshop"],
+        "correct_answer": "Design Studio"
+    },
+    {
+        "question": "What is the name of the test that simulates extreme weather conditions in Test Track?",
+        "answers": ["Environmental Chamber", "Weather Simulation", "Climate Control Test", "Extreme Weather Test"],
+        "correct_answer": "Environmental Chamber"
+    },
+    {
+        "question": "What is the name of the test that measures the car's handling on different road surfaces in Test Track?",
+        "answers": ["Handling Test", "Road Surface Test", "Terrain Test", "Track Test"],
+        "correct_answer": "Handling Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's braking ability in Test Track?",
+        "answers": ["Brake Test", "Stopping Test", "Deceleration Test", "Halt Test"],
+        "correct_answer": "Brake Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's acceleration in Test Track?",
+        "answers": ["Acceleration Test", "Speed Test", "Launch Test", "Velocity Test"],
+        "correct_answer": "Acceleration Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a high-speed loop in Test Track?",
+        "answers": ["High-Speed Loop", "Speed Circuit", "Velocity Loop", "Fast Track"],
+        "correct_answer": "High-Speed Loop"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a winding road in Test Track?",
+        "answers": ["Curvy Road Test", "Winding Road Test", "Twisty Track Test", "Handling Circuit"],
+        "correct_answer": "Winding Road Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a rough road in Test Track?",
+        "answers": ["Rough Road Test", "Bumpy Road Test", "Terrain Test", "Off-Road Test"],
+        "correct_answer": "Rough Road Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a wet road in Test Track?",
+        "answers": ["Wet Road Test", "Rain Simulation Test", "Hydroplane Test", "Water Test"],
+        "correct_answer": "Wet Road Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a dark road in Test Track?",
+        "answers": ["Dark Road Test", "Night Simulation Test", "Visibility Test", "Light Test"],
+        "correct_answer": "Dark Road Test"
+    },
+    {
+        "question": "What is the name of the test that measures the car's performance on a steep hill in Test Track?",
+        "answers": ["Hill Test", "Incline Test", "Slope Test", "Elevation Test"],
+        "correct_answer": "Hill Test"
+    }
+]
